c5a65946946356e5f31eeafa3cdd3e9fb22ef41f
Tiago de Freitas <tiagofreitasp00@gmail.com>
2025-05-23 11:11:08 -0300

    Adjust titles pages

--- src/components/farms/page-farms.tsx
+++ src/components/farms/page-farms.tsx
@@ -10,8 +10,7 @@ export default function FarmsPage() {
 
   return (
     <>
-      <div className="flex items-center justify-between mb-6">
-        <h1 className="text-2xl font-bold">Fazendas</h1>
+      <div className="flex items-center justify-end">
         <FarmForm />
       </div>
 
