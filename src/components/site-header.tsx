c046290f16c5150bc2af33433946c3847de16e04
Tiago de Freitas <tiagofreitasp00@gmail.com>
2025-05-23 11:09:51 -0300

    adjust header dinamic

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,6 +1,19 @@
+'use client'
+
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation"
+
+const routeTitles: Record<string, string> = {
+  "/": "Dashboard",
+  "/farm": "Fazendas",
+  "/producers": "Produtores",
+}
+
 export function SiteHeader() {
+  const pathname = usePathname()
+  const title = routeTitles[pathname as keyof typeof routeTitles]
+
   return (
     <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -9,6 +22,9 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
+        <h1 className="text-lg font-semibold">
+          {title}
+        </h1>
       </div>
     </header>
   )
